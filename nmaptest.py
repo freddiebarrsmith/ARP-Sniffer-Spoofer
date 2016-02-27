2bee228549d5ab9e06c1b2cdf5d741804c9774a9
root <fredbarrsmith@gmail.com>
2016-02-27 15:10:56 +0000

    nmap

--- nmaptest.py
+++ nmaptest.py
@@ -1,22 +1,29 @@
 import nmap
 import json
+
+class ARPhost():
+
+    def __init__(self, name, ip, mac ):
+        ARPhost.__init__(self, name)
+        self.name = name
+        self.ip = ip
+        self.mac = mac
+#    def mac(self, mac):
+
+
 nm = nmap.PortScanner()
 
 host = '192.168.0.*'
 results = nm.scan(hosts=host, arguments='-sP')
 
-#need to parse out results
 
-print results
-
-results2 = nm.all_hosts()
-
-print results2
+tobeparsed = results["scan"]
 
 
-parsed_json = json.loads(results)
-pprint(parsed_json)
-
-#for host in nm.all_hosts():
-#    print nm[host].addresses()
-#    print nm[host].hostnames()
+for host in nm.all_hosts():
+    print tobeparsed[host]
+    print tobeparsed[host]["hostnames"]
+    print "name "
+    print tobeparsed[host]["hostnames"][0]["name"]
+    print tobeparsed[host]["addresses"]["mac"]
+    print tobeparsed[host]["addresses"]["ipv4"]
