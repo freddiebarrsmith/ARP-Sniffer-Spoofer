c4a23b9018f6497769980018f12d44f01c5642ff
root <fredbarrsmith@gmail.com>
2016-03-05 16:08:49 +0000

    working on updating

--- arpsniff.py
+++ arpsniff.py
@@ -3,31 +3,73 @@
 #
 
 
-class ARPhost():
+import nmap
+import json
 
-    def __init__(self, name, ip, mac ):
-        ARPhost.__init__(self, name)
-        self.name =
-        self.ip = 
-        self.mac = 
-#    def mac(self, mac):
-
-#import nmap
-#nm = nmap.PortScanner()
-
-#host = '192.168.0.*'
-#nm.scan(hosts=host, arguments='-sP')
-
-
-
+import time
 import struct
 import socket
 import binascii
-import time
+
+listofhosts = []
+class ARPhost():
+
+    def __init__(self, name, mac, ip):
+#        ARPhost.__init__(self, name, ip, mac)
+        self.name = name
+        self.ip = ip
+        self.mac = mac
+#    def mac(self, mac):
+
+
+nm = nmap.PortScanner()
+
+host = '192.168.0.*'
+results = nm.scan(hosts=host, arguments='-sP')
+
+
+tobeparsed = results["scan"]
+
+
+##all the below is jjust debugging
+
+for host in nm.all_hosts():
+#    print "host status reason"
+#    print tobeparsed[host]["status"]["reason"]
+#    print tobeparsed[host]
+#    print tobeparsed[host]["hostnames"]
+    try:
+#        print "name "
+#        print tobeparsed[host]["hostnames"][0]["name"]
+        name = tobeparsed[host]["hostnames"][0]["name"]
+    except:
+        pass
+  #      print "host name fetching failed"
+    try:
+  #      print "mac address"
+   #     print tobeparsed[host]["addresses"]["mac"]
+        mac = tobeparsed[host]["addresses"]["mac"]
+    except:
+    #    print "mac fetching failed"
+        pass
+    #print tobeparsed[host]["addresses"]["ipv4"]
+    ip = tobeparsed[host]["addresses"]["ipv4"]
+    host = ARPhost(name, mac, ip)
+    listofhosts.append(host)
+
+for host in listofhosts:
+    print host.mac
+    print host.ip
+    print host.name
+#    print nm[host].addresses()
+#    print nm[host].hostnames()
+
+
+
 rawSocket=socket.socket(socket.PF_PACKET,socket.SOCK_RAW,socket.htons(0x0806))
 
 g = open('snifferlog.txt', 'w')
-
+def arpsniff():
 while True:
 
     receivedPacket=rawSocket.recv(66566)
@@ -47,6 +89,6 @@ while True:
     d_addr = socket.inet_ntoa(ipHdr[9]);
 
     if protocol == 62:
-        print protocol
-        print sourceMAC
-        print destinationMAC
+#        print protocol
+#        print sourceMAC
+#        print destinationMAC
